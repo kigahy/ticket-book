f2a1d0033888f936f894c4540d158f450e889ef5
Gahyeon Kim <in47821@gmail.com>
2026-06-22 05:18:29 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/github/kigahy/ticketbook/TicketBookApplication.java
@@ -0,0 +1,13 @@
+package com.github.kigahy.ticketbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kigahy/ticketbook/TicketBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kigahy.ticketbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

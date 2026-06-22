c3b37d4ef439abef5164335ba3926a814e8d227b
Gahyeon Kim <in47821@gmail.com>
2026-06-23 00:33:20 +0900

    feat: 회원가입 기ë구조 설계

--- src/main/java/com/github/kigahy/ticketbook/member/repository/MemberRepository.java
+++ src/main/java/com/github/kigahy/ticketbook/member/repository/MemberRepository.java
@@ -1,4 +1,12 @@
 package com.github.kigahy.ticketbook.member.repository;
 
-public interface MemberRepository {
+import com.github.kigahy.ticketbook.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import java.lang.reflect.Member;
+
+// Repository는 DB 접근용. JpaRepo를 상속받았으므로 비워둬도 돌아감
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    // 이메일 중복검사. "조회용"
+    boolean existsByEmail(String email);
 }
--- src/main/java/com/github/kigahy/ticketbook/member/service/MemberServiceImpl.java
+++ src/main/java/com/github/kigahy/ticketbook/member/service/MemberServiceImpl.java
@@ -1,4 +1,32 @@
 package com.github.kigahy.ticketbook.member.service;
 
-public class MemberServiceImpl {
+
+import com.github.kigahy.ticketbook.member.dto.request.SignupRequest;
+import com.github.kigahy.ticketbook.member.entity.Member;
+import com.github.kigahy.ticketbook.member.repository.MemberRepository;
+//import com.github.kigahy.ticketbook.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    // 객체 바꿔치기, 갈취 방지하기 위한 final 생성자. 멀티스레드 환경에서도 안전성 위함
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void signup(SignupRequest request) {
+        if(memberRepository.existsByEmail(request.getEmail())) {
+            throw new IllegalArgumentException("이미 가입된 이메일입니다.");
+        }
+
+        Member member = new Member(
+                request.getEmail(),
+                request.getPassword(),
+                request.getName()
+        );
+
+        memberRepository.save(member);
+    }
 }

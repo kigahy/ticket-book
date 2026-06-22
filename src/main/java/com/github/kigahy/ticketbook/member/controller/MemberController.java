7537a7554448c43909bb9ca17427f1c7a0ce207a
Gahyeon Kim <in47821@gmail.com>
2026-06-23 02:10:23 +0900

    feat: 회원가입 기능 초안

--- src/main/java/com/github/kigahy/ticketbook/member/controller/MemberController.java
+++ src/main/java/com/github/kigahy/ticketbook/member/controller/MemberController.java
@@ -1,4 +1,21 @@
 package com.github.kigahy.ticketbook.member.controller;
 
+import com.github.kigahy.ticketbook.member.dto.request.SignupRequest;
+import com.github.kigahy.ticketbook.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signup(@RequestBody SignupRequest request) {
+        memberService.signup(request);
+    }
 }

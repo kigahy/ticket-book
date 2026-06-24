ca15c13f00470e83303538aa92e1ba8de3428400
Gahyeon Kim <in47821@gmail.com>
2026-06-24 20:16:03 +0900

    feat: BCrypt 회원가입 암호화 적용

--- src/main/java/com/github/kigahy/ticketbook/member/service/MemberServiceImpl.java
+++ src/main/java/com/github/kigahy/ticketbook/member/service/MemberServiceImpl.java
@@ -1,11 +1,11 @@
 package com.github.kigahy.ticketbook.member.service;
 
-
 import com.github.kigahy.ticketbook.member.dto.request.SignupRequest;
 import com.github.kigahy.ticketbook.member.entity.Member;
 import com.github.kigahy.ticketbook.member.repository.MemberRepository;
 //import com.github.kigahy.ticketbook.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +14,7 @@ public class MemberServiceImpl implements MemberService {
 
     // 객체 바꿔치기, 갈취 방지하기 위한 final 생성자. 멀티스레드 환경에서도 안전성 위함
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void signup(SignupRequest request) {
@@ -21,9 +22,11 @@ public class MemberServiceImpl implements MemberService {
             throw new IllegalArgumentException("이미 가입된 이메일입니다.");
         }
 
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+
         Member member = new Member(
                 request.getEmail(),
-                request.getPassword(),
+                encodedPassword,
                 request.getName()
         );
 

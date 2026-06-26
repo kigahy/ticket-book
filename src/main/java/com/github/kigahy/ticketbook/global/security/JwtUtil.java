805fb59a7c6825a97779ada5cd3eab46a25d1e6c
Gahyeon Kim <in47821@gmail.com>
2026-06-26 19:05:19 +0900

    feat: jwt 검증 구현 테스트 전

--- src/main/java/com/github/kigahy/ticketbook/global/security/JwtUtil.java
+++ src/main/java/com/github/kigahy/ticketbook/global/security/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.github.kigahy.ticketbook.global.security;
 
 import com.github.kigahy.ticketbook.member.entity.Member;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.Date;
 //import java.security.Key; 대신 밑 javax 사용
 import javax.crypto.SecretKey;
+import javax.naming.AuthenticationException;
+
 import io.jsonwebtoken.security.Keys;
 
 @Slf4j
@@ -20,6 +23,7 @@ public class JwtUtil {
     private final JwtProperties jwtProperties;
 
     // secretKey 생성 메서드
+    // jwt properties
     private SecretKey getAuthKey() {
         byte[] keyBytes = jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
@@ -43,4 +47,21 @@ public class JwtUtil {
                 .compact();
     }
     // jwt인증과정 추가했으니 AuthServiceImpl에 accessToken 추가해줌
+
+    // jwt 토큰 검증 메서드
+    // 참거짓으로 판별되니 boolean, 인자는 로그인 서비스로직의 토큰 변수
+    public boolean validateToken(String accessToken) {
+        try {
+            Jwts.parser()
+                    .verifyWith(getAuthKey())
+                    .build()
+                    .parseSignedClaims(accessToken)
+                    .getPayload();
+
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+
+    }
 }

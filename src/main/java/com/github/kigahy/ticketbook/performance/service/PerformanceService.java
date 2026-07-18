b0819617ba073e70ce8f03cd6146c906448f5f96
Gahyeon Kim <in47821@gmail.com>
2026-07-18 23:24:19 +0900

    feat: 공연 조회 서비스로직 시작

--- src/main/java/com/github/kigahy/ticketbook/performance/service/PerformanceService.java
+++ src/main/java/com/github/kigahy/ticketbook/performance/service/PerformanceService.java
@@ -1,4 +1,11 @@
 package com.github.kigahy.ticketbook.performance.service;
 
+import com.github.kigahy.ticketbook.performance.dto.PerformanceResponse;
+
+import java.util.List;
+
 public interface PerformanceService {
+
+    List<PerformanceResponse> getAllPerformances(); // 전체 공연 조회
+    List<PerformanceResponse> getBookablePerformances(); // 예매중인 공연 조회
 }
--- src/main/java/com/github/kigahy/ticketbook/performance/service/PerformanceServiceImpl.java
+++ src/main/java/com/github/kigahy/ticketbook/performance/service/PerformanceServiceImpl.java
@@ -1,4 +1,9 @@
 package com.github.kigahy.ticketbook.performance.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class PerformanceServiceImpl {
 }
